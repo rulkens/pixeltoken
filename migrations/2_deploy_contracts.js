0c52e3fa9957b5d812a5f22d1b46cc684459acf9
Alexander Rulkens <rulkens@gmail.com>
2017-08-08 00:55:15 +0200

    contract migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var PixelToken    = artifacts.require("./PixelToken.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function (deployer) {
+    deployer.deploy(SimpleStorage);
+    deployer.deploy(PixelToken);
 };
