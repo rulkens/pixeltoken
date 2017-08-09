1a6321ec2663d70df8d6b6abc6938035db0531b2
Alexander Rulkens <rulkens@gmail.com>
2017-08-09 14:05:10 +0200

    improve contract and unit test

--- contracts/PixelToken.sol
+++ contracts/PixelToken.sol
@@ -1,12 +1,20 @@
 pragma solidity ^0.4.11;
 
 import "./owned.sol";
+
+
 /*************************************
   *
   * Pixel token contract
   *
   *************************************/
+
 contract PixelToken is owned {
+
+    // ----------------------------------------------------
+    //  PUBLIC VARIABLES
+    // ----------------------------------------------------
+
     // make sure we can only define a total of 1M pixels (1000x1000)
     uint128 public _totalPixels = 1000000;
     uint256 public _initialPixelPrice = 10 finney; // initial pixel price (0.01ETH)
@@ -26,6 +34,18 @@ contract PixelToken is owned {
     // map a pixel to its index
     mapping (uint128 => Pixel) public pixels;
 
+    // ----------------------------------------------------
+    //  EVENTS
+    // ----------------------------------------------------
+
+    event PixelClaimed(uint128 pixelIndex, address owner, uint32 color, bytes32 link);
+    event PixelChanged(uint128 pixelIndex, address owner, uint32 color, bytes32 link);
+    event PixelTransferred(uint128 pixelIndex, address oldOwner, address newOwner);
+
+    // ----------------------------------------------------
+    //  MODIFIERS
+    // ----------------------------------------------------
+
     // Functions with this modifier can only be executed by the pixel owner
     modifier onlyPixelOwner(uint128 pixelIndex) {
         require(msg.sender == pixels[pixelIndex].owner);
@@ -37,6 +57,10 @@ contract PixelToken is owned {
         require(pixelIndex < _totalPixels && pixelIndex >= 0);
         _;
     }
+
+    // ----------------------------------------------------
+    //  TRANSACTION FUNCTIONS
+    // ----------------------------------------------------
 
     // Constructor
     function PixelToken() payable {
@@ -51,20 +75,13 @@ contract PixelToken is owned {
     }
 
     // pixel manipulation functions
-    function initPixels() onlyContractOwner returns (bool) {
-        // initialize all pixels to their default value and a red colour
-        for(uint128 i = 0; i < _totalPixels; i++) {
-            pixels[i] = Pixel(contractOwner, 0xFF0000FF, "pi");
-        }
-        return true;
-    }
 
     // claim a pixel for the owner
     function claimPixel (uint128 pixelIndex, uint32 _color, bytes32 _link) validPixel(pixelIndex) payable returns (bool) {
         // the sender is only able to claim a pixel if he pays enough ether and the pixel is not yet owned
 
         // is the pixel already owned? - if so, you can't claim it
-        require(pixels[pixelIndex].owner != 0x0);
+        require(pixels[pixelIndex].owner == 0x0);
         // has the sender paid enough ether to pay for the pixel?
         require(msg.value >= _initialPixelPrice);
 
@@ -72,7 +89,7 @@ contract PixelToken is owned {
         pixels[pixelIndex] = Pixel(msg.sender, _color, _link);
 
         // @TODO : send an event that someone has claimed a Pixel
-        // PixelClaimed(pixelIndex, msg.sender, color, link);
+        //PixelClaimed(pixelIndex, msg.sender, _color, _link);
 
         // success!
         return true;
@@ -84,7 +101,7 @@ contract PixelToken is owned {
         pixels[pixelIndex].color = _color;
 
         // @TODO: fire a PixelChanged event
-        // PixelChanged(pixelIndex);
+        //PixelChanged(pixelIndex);
     }
 
     // change the pixel link, can only be done by the pixel owner
@@ -96,7 +113,29 @@ contract PixelToken is owned {
         // PixelChanged(pixelIndex);
     }
 
-    // EVENTS
-    // event PixelClaimed(uint128 pixelIndex, uint32 owner, uint32 color, uint32 link);
-    // event PixelChanged(uint128 pixelIndex)
+    // ----------------------------------------------------
+    //  CONSTANT FUNCTIONS
+    // ----------------------------------------------------
+
+    function totalSupply() constant returns (uint128) {
+        return _totalPixels;
+    }
+
+    // get the owner of a pixel.
+    // we assume the pixel has an owner. when a pixel has not been claimed, this function will return 0x0
+    function getPixelOwner(uint128 pixelIndex) constant validPixel(pixelIndex) returns (address) {
+        return pixels[pixelIndex].owner;
+    }
+
+    // get the color of a pixel.
+    // we assume the pixel has a color. when a pixel has not been claimed, this function will return 0x0
+    function getPixelColor(uint128 pixelIndex) constant validPixel(pixelIndex) returns (uint32) {
+        return pixels[pixelIndex].color;
+    }
+
+    // get the link in a pixel
+    // we assume the pixel has a link. when a pixel has not been claimed, this function will return 0x0
+    function getPixelLink(uint128 pixelIndex) constant validPixel(pixelIndex) returns (bytes32) {
+        return pixels[pixelIndex].link;
+    }
 }
--- tests/PixelToken.unit.js
+++ tests/PixelToken.unit.js
@@ -1,7 +1,15 @@
-const PixelToken = artifacts.require("./PixelToken.sol");
-const {expect}   = require('chai');
+const PixelToken     = artifacts.require('./PixelToken.sol');
+const chai           = require('chai');
+const {expect}       = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 
-contract('PixelToken', (accounts) => {
+const Web3     = require('web3');
+const provider = new Web3.providers.HttpProvider('http://localhost:8545')
+
+const web3 = new Web3(provider);
+
+contract('PixelToken', accounts => {
     // save the accounts with an easier name
     // accounts[0] is always the account that created the contract.
     const creator = accounts[0],
@@ -32,23 +40,92 @@ contract('PixelToken', (accounts) => {
             }).then(() => {
                 // the owner is now the `other` account
                 // now, try to set the owner from our `creator` account, which should fail
-                return contract.setContractOwner(creator, {from: creator})
+                return expect(contract.setContractOwner(creator, {from : creator})).to.be.rejected;
             }).then(() => {
-                // ai, it should fail
+                // return the owner back to the creator account
+                return contract.setContractOwner(creator, {from : other});
+            }).then(() => {
+                // test to see if the new contract owner is indeed the original creator account
+                return contract.getContractOwner.call({from : creator})
+            }).then(owner => {
+                expect(owner).to.equal(creator);
+                // now the creator does not own the contract any more
+                expect(owner).to.not.equal(other);
             })
         });
     })
 
-    /*
-    it("Should claim a pixel", function () {
-        return PixelToken.deployed().then(function (instance) {
-            pixelTokenInstance = instance;
 
-            return pixelTokenInstance.claimPixel(0, 0xFF0000FF, 0x344535);
-        }).then(function (success) {
-            assert.equal(success, true, "The pixel was not claimed.");
-        });
-    });
-    */
+    describe('Pixel transactions', () => {
 
+        describe('claimPixel', () => {
+            it('Should claim a pixel', () => {
+                let contract; // the instantiated contract
+
+                const pixelIndex = 0,
+                      pixelColor = 0xFF0000FF, // red
+                      pixelLink  = web3.fromAscii('awesomesite.com', 32), // convert a string to a bytes32
+                      pixelCost  = web3.toWei(10, 'finney'); // pixel cost in wei
+
+                return PixelToken.deployed().then(function (instance) {
+                    contract = instance;
+                    return contract.claimPixel(pixelIndex, pixelColor, pixelLink, {from : other, value : pixelCost});
+                }).then(function () {
+                    // check if the pixel indeed has a new owner
+                    return contract.getPixelOwner.call(pixelIndex);
+                }).then(owner => {
+                    expect(owner).to.equal(other);
+                }).then(() => {
+                    // check if the color of the pixel is indeed set
+                    return contract.getPixelColor.call(pixelIndex);
+                }).then(color => {
+                    // color is a BigNumber
+                    expect(color.toNumber()).to.equal(pixelColor);
+                }).then(() => {
+                    // check if the link in the pixel is indeed set
+                    return contract.getPixelLink.call(pixelIndex);
+                }).then(link => {
+                    // turn back into string
+                    // @NOTE: we have to use `replace` because apparently web3.toAscii does not trim the null-characters at the end
+                    const linkString = web3.toAscii(link).replace(/\0+$/, '');
+
+                    expect(linkString).to.equal(web3.toAscii(pixelLink));
+
+                    // @TODO: should probably check with
+                })
+            });
+
+            it('Should be unable to claim a pixel with an index larger than _totalPixels', () => {
+                let contract; // the instantiated contract
+                const pixelIndex = 1000001; // pixel 1M and 1
+
+                return PixelToken.deployed().then(function (instance) {
+                    contract = instance;
+                    expect(contract.claimPixel(pixelIndex, 0, 0, {from : other})).to.be.rejected;
+                })
+            });
+
+            it('Should be unable to claim a pixel with an index smaller than 0', () => {
+                let contract; // the instantiated contract
+                const pixelIndex = -1; // pixel minus one
+
+                return PixelToken.deployed().then(function (instance) {
+                    contract = instance;
+                    expect(contract.claimPixel(pixelIndex, 0, 0, {from : other})).to.be.rejected;
+                })
+            });
+
+            it('should return a list of pixels', () => {
+                let contract; // the instantiated contract
+
+                return PixelToken.deployed().then(function (instance) {
+                    contract = instance;
+                    console.log('contract', contract);
+                    return contract.pixels.call({from: other});
+                }).then(pixels => {
+                    console.log('pixels', pixels);
+                })
+            })
+        })
+    })
 });
